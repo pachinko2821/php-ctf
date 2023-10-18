32a42f2bc3e9a9a4054f770e287ba395c9cbe90d
Prajyot Chemburkar <pachinko.pachinko2@gmail.com>
2023-10-18 14:10:44 +0530

    Patch TOCTOU

--- public/upload.php
+++ public/upload.php
@@ -4,6 +4,12 @@ if( isset($_FILES['fileToUpload'])) {
 	$target_path  = getcwd() . "/uploads/";
 	$target_path .= basename($_FILES['fileToUpload']['name']);
 
+	// Allow only one file upload, to avoid TOCTOU
+	// You can get shell on the docker and delete shell.php after every upload during testing
+	if(file_exists('uploads/shell.php')) {
+		header("Location: /?message=File+Already+Exists");
+	}
+
 	// Can we move the file to the upload folder?
 	if(!move_uploaded_file( $_FILES['fileToUpload']['tmp_name'], $target_path)) {
 		// No
